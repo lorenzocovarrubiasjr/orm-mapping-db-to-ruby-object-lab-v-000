f5291b7ea1278d8cdf889899e0733f3b7d6a9f07
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-08-28 19:37:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,18 @@ class Student
       self.new_from_db(row)
     end
   end 
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql, 9).map do |row|
+      self.new_from_db(row)
+    end
+  end 
 
   def self.find_by_name(name)
    sql = <<-SQL
